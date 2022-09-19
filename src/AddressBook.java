11c047ba1398a50cd06c02b0fa1c90d15aac36ba
Haghighi <83563347+jaydonhaghighi@users.noreply.github.com>
2022-09-19 16:33:10 -0400

    AddressBook.java added methods addBuddy and removeBuddy which can add and remove people from addressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,13 +1,9 @@
 public class AddressBook {
 
-    public void addBuddy(BuddyInfo buddyInfo) {
-    }
-    public void removeBuddy(BuddyInfo buddyInfo) {
-    }
+
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Jaydon", "Carleton", "613-999-9999");
         AddressBook addressBook = new AddressBook();
-        addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+
     }
 }
