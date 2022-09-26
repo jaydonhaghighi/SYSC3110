56014d24135ff1636c16656af0f3c5cf5cf2ff73
Haghighi <83563347+jaydonhaghighi@users.noreply.github.com>
2022-09-26 19:30:36 -0400

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,8 +6,6 @@ public class AddressBook {
     private List<BuddyInfo> addressBookList = new ArrayList<BuddyInfo>();
     public static void main(String[] args) {
         BuddyInfo buddy0 = new BuddyInfo("Jaydon", "Carleton", "613-999-9999");
-        BuddyInfo buddy1 = new BuddyInfo("Jaydon", "Carleton", "613-999-9999");
-        BuddyInfo buddy2 = new BuddyInfo("Jaydon", "Carleton", "613-999-9999");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy0);
         addressBook.addBuddy(buddy1);
